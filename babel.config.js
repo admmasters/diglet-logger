fbf7eee3b197e6e9979a145360fa878fce818ccb
Matt Revell <matthew.revell@zopa.com>
2021-05-18 13:45:54 +0100

    add react preset to babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript"
-  ]
+module.exports = (api) => {
+  api.cache(false);
+  return {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-typescript',
+    ],
+  };
 };
