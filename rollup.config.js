08a876e4cc2181d8197ecc3bee8cc3a69e757c44
Matt Revell <matthew.revell@zopa.com>
2021-05-18 13:49:59 +0100

    simplify the rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,21 @@
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
-import pkg from "./package.json";
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import pkg from './package.json';
 
-const name = "RollupFresh";
-
-const extensions = [".js", ".jsx", ".ts", ".tsx"];
-
-export default [
-  {
-    input: "src/index.ts",
-    external: [],
-    plugins: [
-      resolve({ extensions }),
-      commonjs(),
-      babel({ extensions, include: ["src/**/*"] })
-    ],
-    output: [
-      {
-        file: pkg.main,
-        format: "cjs"
-      },
-      {
-        file: pkg.module,
-        format: "es"
-      },
-      {
-        file: pkg.browser,
-        format: "iife",
-        name,
-
-        // https://rollupjs.org/guide/en#output-globals-g-globals
-        globals: {}
-      }
-    ]
-  }
-];
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  external: ['react', 'react-dom'],
+  plugins: [nodeResolve(), typescript(), commonjs(), json()],
+};
